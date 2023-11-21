46b0057125b97407fd9767f4bd721c75bd37687b
Thakshin N <thakshinnaidoo@gmail.com>
2023-11-21 07:12:18 +0200

    Multilevel Quiz working

--- MVVM/View/FindingCallNumbersView.xaml.cs
+++ MVVM/View/FindingCallNumbersView.xaml.cs
@@ -10,6 +10,8 @@ namespace LibraryDeweyDecimalApp.MVVM.View
 {
     public partial class FindingCallNumbersView : UserControl
     {
+        private int quizCount = 0;
+        private int level = 1;
         private DeweyDecimalLibrary deweyLibrary;
         private Dictionary<string, string> answerPair = new Dictionary<string, string>();
 
@@ -17,7 +19,7 @@ namespace LibraryDeweyDecimalApp.MVVM.View
         {
             InitializeComponent();
             LoadDeweyData();
-            InitializeQuizOptions();
+            InitializeQuizOptions(level);
         }
 
         private void LoadDeweyData()
@@ -71,37 +73,81 @@ namespace LibraryDeweyDecimalApp.MVVM.View
         {
             var thirdLevelDescriptions = new List<string>();
 
-            foreach (var subcategory in topLevelCategory.Children)
+            if (topLevelCategory.Children != null && topLevelCategory.Children.Any())
             {
-                foreach (var subsubcategory in subcategory.Children)
+                foreach (var subcategory in topLevelCategory.Children)
                 {
-                    thirdLevelDescriptions.Add(subsubcategory.Description);
+                    // Check if the subcategory has children
+                    if (subcategory.Children != null && subcategory.Children.Any())
+                    {
+                        foreach (var subsubcategory in subcategory.Children)
+                        {
+                            thirdLevelDescriptions.Add(subsubcategory.Description);
+                        }
+                    }
+                    else
+                    {
+                        // If no children, add the subcategory's description directly
+                        thirdLevelDescriptions.Add(subcategory.Description);
+                    }
                 }
+            }
+            else
+            {
+                // If no children, add the subcategory's description directly
+                thirdLevelDescriptions.Add(topLevelCategory.Description);
             }
 
             return thirdLevelDescriptions;
         }
 
-
-        private void InitializeQuizOptions()
+        private void InitializeQuizOptions(int level)
         {
             Random random = new Random();
             int randomNumber = random.Next(0, 4); // Generates random number between 0 and 3 (inclusive)
 
-            // Get top-level categories
-            var topLevelCategories = deweyLibrary.Root;
+            List<DeweyDecimalCategory> quizOptions;
 
-            // Shuffle the categories to get random options
-            var quizOptions = topLevelCategories.OrderBy(x => Guid.NewGuid()).ToList();
+            // Select categories based on the specified level
+            switch (level)
+            {
+                case 1:
+                    quizOptions = deweyLibrary.Root.OrderBy(x => Guid.NewGuid()).ToList();
+                    break;
+
+                case 2:
+                    // Select second-level categories
+                    var secondLevelCategories = deweyLibrary.Root
+                        .SelectMany(category => category.Children)
+                        .OrderBy(x => Guid.NewGuid())
+                        .ToList();
+                    quizOptions = secondLevelCategories;
+                    break;
+
+                case 3:
+                    // Select third-level categories
+                    var thirdLevelCategories = deweyLibrary.Root
+                        .SelectMany(category => category.Children)
+                        .SelectMany(subcategory => subcategory.Children)
+                        .OrderBy(x => Guid.NewGuid())
+                        .ToList();
+                    quizOptions = thirdLevelCategories;
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(level), "Invalid level specified");
+            }
 
             // Display options to the user
             Option1.Content = $"{quizOptions[0].Number} - {quizOptions[0].Description}";
             Option2.Content = $"{quizOptions[1].Number} - {quizOptions[1].Description}";
             Option3.Content = $"{quizOptions[2].Number} - {quizOptions[2].Description}";
             Option4.Content = $"{quizOptions[3].Number} - {quizOptions[3].Description}";
-            
+
+            // Display random description based on the selected category
             DisplayRandomDescription(quizOptions[randomNumber]);
         }
+
 
         private void CheckAnswer(object sender, RoutedEventArgs e)
         {
@@ -111,19 +157,39 @@ namespace LibraryDeweyDecimalApp.MVVM.View
             if (AreStringsAPair(answerPair, selectedOption, randomDescription))
             {
                 // User selected the correct answer
-                MessageBox.Show("Correct! Moving to the next level.");
+                var message = "";
+                if (level < 3)
+                {
+                    level++;
+                    message = ("Correct! Moving to the next level.");
+                    InitializeQuizOptions(level);
+                }
+                else
+                {
+                    level = 1;
+                    quizCount++;
+                    message = ("Correct! You completed the quiz.");
+                    
+                }
 
-                // Display a new random description and update quiz options
-                InitializeQuizOptions();
+                if (quizCount == 10)
+                {
+                    MessageBox.Show("Congratulations! You completed 10 quizzes.");
+                }
+
+                MessageBox.Show(message);
+                InitializeQuizOptions(level);
             }
             else
             {
+                level = 1;
+
                 // User selected the wrong answer
                 var answer = $"{answerPair.FirstOrDefault()}";
                 MessageBox.Show($"Incorrect! Answer: {answer}");
 
                 // Display the same random description but with new quiz options
-                InitializeQuizOptions();
+                InitializeQuizOptions(level);
             }
         }
 
